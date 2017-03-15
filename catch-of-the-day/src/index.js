ae024c78749448c8839e716fe98366d7d65911b5
arjunkhode <arjunkhode@gmail.com>
2017-03-14 22:45:36 -0700

    Routed store to actual menu

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -4,5 +4,19 @@ import {render} from 'react-dom';
 import './css/style.css';
 import App from './components/App';
 import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
+import {BrowserRouter, Match, Miss} from 'react-router';
 
-render(<App/>, document.querySelector('#main'));
+const Root = ()=>{
+	return(
+		<BrowserRouter>
+		<div>
+			<Match exactly pattern="/" component={StorePicker}/>
+			<Match exactly pattern="/store/:storeId" component={App}/>
+			<Miss component={NotFound}/>
+		</div>
+		</BrowserRouter>
+		)
+}
+
+render(<Root/>, document.querySelector('#main'));
