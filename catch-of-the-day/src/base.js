0498934b16dce0f554b322daf74255e68d7c266b
arjunkhode <arjunkhode@gmail.com>
2017-03-17 23:27:19 -0700

    State synced with firebase

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base=Rebase.createClass({
+	apiKey: "AIzaSyDaJmaCg9xXFY1Ylui42IYzlxtjhVkjqw0",
+    authDomain: "cotd-arjun-khode.firebaseapp.com",
+    databaseURL: "https://cotd-arjun-khode.firebaseio.com",
+});
+
+export default base;
